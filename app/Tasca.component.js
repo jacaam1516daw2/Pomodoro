71a847c18b114de51a84965b370cbf732227ba87
jaimecastignani <jaimecastignani@gmail.com>
2016-03-05 11:01:36 +0100

    Presetntable 1

--- app/Tasca.component.js
+++ app/Tasca.component.js
@@ -8,21 +8,27 @@
         .Class({
             constructor: function () {
                 this.tasques = [];
-                this.min = 24;
-                this.seg = 59;
+                this.min = 0;
+                this.seg = 3;
+                this.fin = 0;
+                var a = new Date();
+                this.data = a;
                 this.stateTime = 0;
                 this.afegirTasca = function (tascaName) {
                     if (tascaName != '') {
                         idTask++;
-                        this.model = new app.Tasca(idTask, tascaName, 24, 59, 0);
+                        this.model = new app.Tasca(idTask, tascaName, 0, 3, this.data, 0, 0);
                         sessionStorage.setItem(idTask, JSON.stringify(this.model));
                         this.tasques.push(this.model);
                     }
+                    /*this.tasques = [];
+                    for (var key in sessionStorage) {
+                        this.tasques.push(JSON.parse(sessionStorage.getItem(key)));
+                    }*/
                 };
                 this.esborrarTasca = function (id) {
                     var tasca = JSON.parse(sessionStorage.getItem(id));
                     sessionStorage.removeItem(id);
-                    //this.tasques.splice(this.tasques.indexOf(tasca), 1);
                     this.tasques = [];
                     for (var key in sessionStorage) {
                         this.tasques.push(JSON.parse(sessionStorage.getItem(key)));
@@ -37,8 +43,12 @@
                     tasca.stateTime = 0;
                     sessionStorage.removeItem(id);
                     sessionStorage.setItem(id, JSON.stringify(tasca));
-                    this.tasques.splice(this.tasques.indexOf(tasca), 1);
-                    this.tasques.push(tasca);
+                    /* this.tasques.splice(this.tasques.indexOf(tasca), 1);
+                       this.tasques.push(tasca);*/
+                    this.tasques = [];
+                    for (var key in sessionStorage) {
+                        this.tasques.push(JSON.parse(sessionStorage.getItem(key)));
+                    }
                 };
                 this.startTime = function () {
                     this.stateTime = 1;
@@ -47,17 +57,17 @@
                     this.stateTime = 2;
                 };
                 this.resetTime = function () {
-                    this.min = 24;
-                    this.seg = 59;
+                    this.min = 0;
+                    this.seg = 3;
                     this.stateTime = 0;
                 };
                 this.afegirTimer = function (id) {
-                    var t = JSON.parse(sessionStorage.getItem(id));
-                    this.id = t.id;
-                    this.task = t.task;
-                    this.min = t.min;
-                    this.seg = t.seg;
-                    this.stateTime = t.stateTime;
+                    var tasca = JSON.parse(sessionStorage.getItem(id));
+                    this.id = tasca.id;
+                    this.task = tasca.task;
+                    this.min = tasca.min;
+                    this.seg = tasca.seg;
+                    this.stateTime = tasca.stateTime;
                 };
                 setInterval(() => {
                     if (this.stateTime == 1) {
@@ -68,11 +78,18 @@
                         }
                         if (this.seg == 0 && this.min == 0) {
                             this.stateTime = 2;
+                            this.fin = 1;
                         }
                     }
                 }, 1000);
 
                 this.enviat = false;
+            },
+            transform: function (v, args) {
+                v.forEach(function (item, index) {
+                    v[index] = item.toUpperCase();
+                })
+                return v.sort().join(args[0]);
             },
             valida: function () {
                 this.enviat = true;
--- app/Tasca.model.js
+++ app/Tasca.model.js
@@ -1,11 +1,13 @@
 (function (app) {
     app.Tasca = Tasca;
 
-    function Tasca(id, task, min, seg, stateTime) {
+    function Tasca(id, task, min, seg, data, fin, stateTime) {
         this.id = id;
         this.task = task;
         this.min = min;
         this.seg = seg;
+        this.data = data;
+        this.fin = fin;
         this.stateTime = stateTime;
     }
 })(window.app || (window.app = {}));
